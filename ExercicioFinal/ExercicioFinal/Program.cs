cb5fd01124b63ca7fa9a0e22ed5313e2f8cc966a
raulhf <raul.rhf9316@gmail.com>
2023-05-06 19:58:52 -0300

    transformando requisitos em metodos

--- ExercicioFinal/ExercicioFinal/Program.cs
+++ ExercicioFinal/ExercicioFinal/Program.cs
@@ -17,9 +17,9 @@ namespace ExercicioFinal
                 List<string[]> studentExams = new List<string[]>();
 
                 //MOCKTEST
-                string[] test = new string[] { "RAUL", "PROWAY", "10" };
-                string[] test2 = new string[] { "TASSIA", "PROWAY", "10" };
-                string[] test3 = new string[] { "JOÃO", "PROWAY", "0" };
+                string[] test = new string[] { "RAUL", "CSHARP", "10" };
+                string[] test2 = new string[] { "TASSIA", "CSHARP", "10" };
+                string[] test3 = new string[] { "JOÃO", "CSHARP", "0" };
                 studentExams.Add(test);
                 studentExams.Add(test2);
                 studentExams.Add(test3);
@@ -31,14 +31,7 @@ namespace ExercicioFinal
 
                 if (opt == 1)
                 {
-                    Console.WriteLine("\n1 - Para cadastrar questoes e respostas");
-                    Console.WriteLine("2 - Para consultar notas de alunos");
-                    Console.WriteLine("3 - Para consultar maior e menor acerto");
-                    Console.WriteLine("4 - Para consultar o total de alunos que utilizaram o sistema;,");
-                    Console.WriteLine("5 - Para consultar a média de notas da turma");
-                    Console.WriteLine("6 - Para consultar o gabarito");
-                    Console.WriteLine("0 - Para voltar ao menu principal");    
-                    int optTeacher = int.Parse(Console.ReadLine());
+                    int optTeacher = Proof.TeacherMenu();
 
                     switch (optTeacher)
                     {
@@ -47,84 +40,32 @@ namespace ExercicioFinal
                             Console.WriteLine("Digite o numero de questões que deseja cadastrar:");
                             int qtt = int.Parse(Console.ReadLine());
                             Proofs.Add(Proof.RegisterQuestions(qtt));
+
                             break;
 
                         case 2:
-                            for (int i = 0; i < studentExams.Count; i++)
-                            {
-                                Console.WriteLine("\nNome: " + studentExams[i][0]);
-                                Console.WriteLine("Curso: " + studentExams[i][1]);
-                                Console.WriteLine("Nota: " + studentExams[i][2]);
-                            }
+                            Proof.CheckClassNotes(studentExams);
+
                             break;
 
                         case 3:
-                            int maxValue = 0;
-                            string[] bestGrade = new string[] { };
-                            int minValue = 10;
-                            string[] worstGrade = new string[] { };
-
-                            for (int i = 0; i < studentExams.Count; i++)
-                            {
-                                if (int.Parse(studentExams[i][2]) > maxValue)
-                                {                                   
-                                    maxValue = int.Parse(studentExams[i][2]);
-                                    bestGrade = studentExams[i];
-                                }
-                                if (int.Parse(studentExams[i][2]) < minValue)
-                                {                                   
-                                    minValue = int.Parse(studentExams[i][2]);
-                                    worstGrade = studentExams[i];
-                                }
-                            }
-
-                            for (int i = 0; i <= 0; i++)
-                            {
-                                Console.WriteLine("\nMelhor nota:");
-                                Console.WriteLine("Nome: " + bestGrade[0]);
-                                Console.WriteLine("Curso: " + bestGrade[1]);
-                                Console.WriteLine("Nota: " + bestGrade[2]);
-
-                                Console.WriteLine("\nPior nota:");
-                                Console.WriteLine("Nome: " + worstGrade[0]);
-                                Console.WriteLine("Curso: " + worstGrade[1]);
-                                Console.WriteLine("Nota: " + worstGrade[2]);
-                            }
+                            Proof.CheckBiggerAndSmallerGrade(studentExams);
+                            
                             break; 
 
                         case 4:
                             Console.WriteLine("\nO total de alunos que utilizão o sistema é: " + studentExams.Count);
+
                             break; 
 
                         case 5:
-                            List<int> gradeList = new List<int>();
-
-                            for (int i = 0; i < studentExams.Count; i++)
-                            {
-                                gradeList.Add(int.Parse(studentExams[i][2]));
-                            }
-                            double media = gradeList.Sum() / gradeList.Count;
-                            Console.WriteLine("A media da turma é : " +  media);
+                            Proof.CheckClassAverage(studentExams);
+                            
                             break; 
 
                         case 6:
-                            for (int i = 0; i <= 0; i++)
-                            {
-                                Console.WriteLine("______________________________________\nINFORMAÇÕES DA PROVA : ");
-                                Console.WriteLine("Nome do ministrador da prova: " + questionsAndAnswers[0][0]);
-                                Console.WriteLine("Curso: " + questionsAndAnswers[0][1]);
-                                Console.WriteLine("\nQUESTÕES DA PROVA: ");
-                                foreach (var item in questionsAndAnswers[1])
-                                {
-                                    Console.WriteLine(item);
-                                }
-                                Console.WriteLine("\nGABARITO: ");
-                                foreach (var item in questionsAndAnswers[2])
-                                {
-                                    Console.Write(item + " - ");
-                                }
-                                Console.WriteLine("\n______________________________________");
-                            }
+                            Proof.CheckAssessmentInformation(questionsAndAnswers);
+                            
                             break; 
                         case 0:
                             break; 
@@ -132,9 +73,9 @@ namespace ExercicioFinal
                 }
                 else if (opt == 2)
                 {
-                    Console.WriteLine("\n1 - Para realizar prova");
-                    Console.WriteLine("2 - Para consultar sua nota");
-                    Console.WriteLine("0 - Para voltar ao menu principal");
+                    Console.WriteLine("\n1 - Para realizar PROVA");
+                    Console.WriteLine("2 - Para consultar sua NOTA");
+                    Console.WriteLine("Qualqur outro numero para voltar ao menu principal");
                     int optStudent = int.Parse(Console.ReadLine());
 
                     if (optStudent == 1)
@@ -142,18 +83,7 @@ namespace ExercicioFinal
                         Console.WriteLine("Nome do aluno:");
                         string studentsName = Console.ReadLine().ToUpper();
 
-                        Console.WriteLine("\nCURSO\nDigite a opção desejada:\n1 - CSHARP \n2 - FLUTTER\n3 - DEVOPS\n4 - JAVA");
-                        string subject = Console.ReadLine();
-
-                        switch (subject)
-                        {
-                            case "1": subject = "CSHARP"; break;
-                            case "2": subject = "FLUTTER"; break;
-                            case "3": subject = "DEVOPS"; break;
-                            case "4": subject = "JAVA"; break;
-                            default: break;
-
-                        }
+                        string subject = Proof.ChooseCourse();
 
                         string[] studentTest = Proof.ApplyTest(studentsName, subject, questionsAndAnswers[1], questionsAndAnswers[2]);
 
@@ -161,22 +91,7 @@ namespace ExercicioFinal
                     }
                     else if(optStudent == 2)
                     {
-                        Console.WriteLine("Nome do aluno:");
-                        string studentsName = Console.ReadLine().ToUpper();
-
-                        Console.WriteLine("Nome do Curso:");
-                        string subject = Console.ReadLine().ToUpper();
-
-                        for (int i = 0; i < studentExams.Count; i++)
-                        {
-                            if (studentExams[i][0] == studentsName && studentExams[i][1] == subject)
-                            {
-                                Console.WriteLine("Nome: " + studentExams[i][0]);
-                                Console.WriteLine("Curso: " + studentExams[i][1]);
-                                Console.WriteLine("Nota: " + studentExams[i][2]);
-                            }
-                            
-                        }
+                        Proof.CheckStudentGrade(studentExams);                        
                     }
                 }
                 else
@@ -185,8 +100,6 @@ namespace ExercicioFinal
                 }
 
             }
-            
-
             Console.ReadKey();
         }
     }
--- ExercicioFinal/ExercicioFinal/Proof.cs
+++ ExercicioFinal/ExercicioFinal/Proof.cs
@@ -32,18 +32,8 @@ namespace ExercicioFinal
                     string teacher = Console.ReadLine();
                     subjectAndTeacher[0] = teacher;
 
-                    Console.WriteLine("\nCURSO\nDigite a opção desejada:\n1 - CSHARP \n2 - FLUTTER\n3 - DEVOPS\n4 - JAVA");
-                    string subject = Console.ReadLine();
+                    string subject = ChooseCourse();
 
-                    switch (subject)
-                    {
-                        case "1": subject = "CSHARP"; break;
-                        case "2": subject = "FLUTTER"; break;
-                        case "3": subject = "DEVOPS"; break;
-                        case "4": subject = "JAVA"; break;
-                        default: break;
-
-                    }
                     subjectAndTeacher[1] = subject;
                 }
                 for (int i = 0; i <= totalQuestions - 1; i++)
@@ -83,6 +73,134 @@ namespace ExercicioFinal
             string[] studentInfo = new string[] { student, subject, fullMark.ToString() };
 
             return studentInfo;
+        }
+
+        public static string ChooseCourse()
+        {
+            Console.WriteLine("\nCURSO\nDigite a opção desejada:\n1 - CSHARP \n2 - FLUTTER\n3 - DEVOPS\n4 - JAVA");
+            string subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "1": subject = "CSHARP"; break;
+                case "2": subject = "FLUTTER"; break;
+                case "3": subject = "DEVOPS"; break;
+                case "4": subject = "JAVA"; break;
+                default: break;
+            }
+
+            return subject;
+        }
+        public static int TeacherMenu()
+        {
+            Console.WriteLine("\n1 - Para cadastrar PROVA");
+            Console.WriteLine("2 - Para consultar NOTAS DOS ALUNOS");
+            Console.WriteLine("3 - Para consultar MAIOR E MENOR NOTA");
+            Console.WriteLine("4 - Para consultar o TOTAL DE ALUNOS QUE REALIZRAM A PROVA");
+            Console.WriteLine("5 - Para consultar a MEDIA DA TURMA");
+            Console.WriteLine("6 - Para consultar o GABARITO");
+            Console.WriteLine("0 - Para voltar ao menu principal");
+            int optTeacher = int.Parse(Console.ReadLine());
+
+            return optTeacher;
+        }
+
+        public static void CheckBiggerAndSmallerGrade(List<string[]> studentExams)
+        {
+            int maxValue = 0;
+            string[] bestGrade = new string[] { };
+
+            int minValue = 10;
+            string[] worstGrade = new string[] { };
+
+            for (int i = 0; i < studentExams.Count; i++)
+            {
+                if (int.Parse(studentExams[i][2]) > maxValue)
+                {
+                    maxValue = int.Parse(studentExams[i][2]);
+                    bestGrade = studentExams[i];
+                }
+                if (int.Parse(studentExams[i][2]) < minValue)
+                {
+                    minValue = int.Parse(studentExams[i][2]);
+                    worstGrade = studentExams[i];
+                }
+            }
+
+            for (int i = 0; i <= 0; i++)
+            {
+                Console.WriteLine("\nMaior nota:");
+                Console.WriteLine("Nome: " + bestGrade[0]);
+                Console.WriteLine("Curso: " + bestGrade[1]);
+                Console.WriteLine("Nota: " + bestGrade[2]);
+
+                Console.WriteLine("\nMenor nota:");
+                Console.WriteLine("Nome: " + worstGrade[0]);
+                Console.WriteLine("Curso: " + worstGrade[1]);
+                Console.WriteLine("Nota: " + worstGrade[2]);
+            }
+        }
+
+        public static void CheckClassNotes(List<string[]> studentExams)
+        {
+            for (int i = 0; i < studentExams.Count; i++)
+            {
+                Console.WriteLine("\nNome: " + studentExams[i][0]);
+                Console.WriteLine("Curso: " + studentExams[i][1]);
+                Console.WriteLine("Nota: " + studentExams[i][2]);
+            }
+        }
+
+        public static void CheckClassAverage(List<string[]> studentExams)
+        {
+            List<int> gradeList = new List<int>();
+
+            for (int i = 0; i < studentExams.Count; i++)
+            {
+                gradeList.Add(int.Parse(studentExams[i][2]));
+            }
+            double media = gradeList.Sum() / gradeList.Count;
+            Console.WriteLine("A media da turma é : " + media);
+        }
+
+        public static void CheckAssessmentInformation(List<string[]> questionsAndAnswers)
+        {
+            for (int i = 0; i <= 0; i++)
+            {
+                Console.WriteLine("______________________________________\nINFORMAÇÕES DA PROVA : ");
+                Console.WriteLine("Nome do ministrador da prova: " + questionsAndAnswers[0][0]);
+                Console.WriteLine("Curso: " + questionsAndAnswers[0][1]);
+                Console.WriteLine("\nQUESTÕES DA PROVA: ");
+                foreach (var item in questionsAndAnswers[1])
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("\nGABARITO: ");
+                foreach (var item in questionsAndAnswers[2])
+                {
+                    Console.Write(item + " - ");
+                }
+                Console.WriteLine("\n______________________________________");
+            }
+        }
+
+        public static void CheckStudentGrade(List<string[]> studentExams)
+        {
+            Console.WriteLine("Nome do aluno:");
+            string studentsName = Console.ReadLine().ToUpper();
+
+            string subject = ChooseCourse();
+
+            for (int i = 0; i < studentExams.Count; i++)
+            {
+                if (studentExams[i][0] == studentsName && studentExams[i][1] == subject)
+                {
+                    Console.WriteLine("Nome: " + studentExams[i][0]);
+                    Console.WriteLine("Curso: " + studentExams[i][1]);
+                    Console.WriteLine("Nota: " + studentExams[i][2]);
+                }
+
+            }
         }
     }
 }
